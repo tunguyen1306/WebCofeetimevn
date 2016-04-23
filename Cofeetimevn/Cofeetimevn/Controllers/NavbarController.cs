d87010670228fca6c7b03bdd8bb7dc44715e4b3b
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-04-23 15:53:56 +0700

    New Web

---
+++ Cofeetimevn/Cofeetimevn/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using Cofeetimevn.Domain;
+using Cofeetimevn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cofeetimevn.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
---
+++ Cofeetimevn/Cofeetimevn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cofeetimevn.MvcApplication" Language="C#" %>
